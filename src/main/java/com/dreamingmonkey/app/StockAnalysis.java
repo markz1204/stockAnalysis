6a5be1adbd50749d9d2226c1db7afcdc60bca3f2
dreamingmonkey <zyl1204@gmail.com>
2017-06-07 17:04:44 +1000

    Description: use current price compare with average 20.

--- src/main/java/com/dreamingmonkey/app/StockAnalysis.java
+++ src/main/java/com/dreamingmonkey/app/StockAnalysis.java
@@ -63,20 +63,22 @@ public class StockAnalysis {
 
             historicalQuoteList = historicalQuoteList.parallelStream().filter(hq->(hq.getVolume() != null && hq.getVolume() > 0)).sorted(Comparator.comparing(HistoricalQuote::getDate).reversed()).collect(Collectors.toList());
 
-            if (historicalQuoteList.size() < 20) {
+            if (historicalQuoteList.size() < 21) {
                 return false;
             }
 
+            HistoricalQuote latest = historicalQuoteList.get(0);
+
             //Get recent 20 days quotes.
-            historicalQuoteList = historicalQuoteList.subList(0, 20);
+            historicalQuoteList = historicalQuoteList.subList(1, 21);
 
             OptionalDouble average20 = historicalQuoteList.stream().mapToDouble(hq -> hq.getAdjClose() != null ? hq.getAdjClose().doubleValue() : 0.00).average();
 
             if(average20.getAsDouble() >= Double.valueOf(MINI_PRICE) && average20.getAsDouble() <= Double.valueOf(MAX_PRICE)) {
                 if (isAbove20Avg) {
-                    return historicalQuoteList.get(0).getAdjClose().doubleValue() > average20.getAsDouble() && historicalQuoteList.get(1).getAdjClose().doubleValue() < average20.getAsDouble();
+                    return latest.getAdjClose().doubleValue() > average20.getAsDouble() && historicalQuoteList.get(0).getAdjClose().doubleValue() < average20.getAsDouble();
                 } else {
-                    return historicalQuoteList.get(0).getAdjClose().doubleValue() < average20.getAsDouble() && historicalQuoteList.get(1).getAdjClose().doubleValue() > average20.getAsDouble();
+                    return latest.getAdjClose().doubleValue() < average20.getAsDouble() && historicalQuoteList.get(0).getAdjClose().doubleValue() > average20.getAsDouble();
                 }
             }
 
